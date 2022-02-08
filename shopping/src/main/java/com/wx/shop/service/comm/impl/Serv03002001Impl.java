36ff8f58541f624ed8b81e38bca5bdd6d6930f03
vincentenxy <vincentenxy@outlook.com>
2022-02-09 00:08:20 +0800

    1、修改文件上传类 2、修改前后台接口文档，文件上传接口描述 3、增加测试环境配置文件

--- shopping/src/main/java/com/wx/shop/service/comm/impl/Serv03002001Impl.java
+++ shopping/src/main/java/com/wx/shop/service/comm/impl/Serv03002001Impl.java
@@ -1,8 +1,10 @@
 package com.wx.shop.service.comm.impl;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.wx.shop.service.comm.Serv03002001;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +22,7 @@ import java.util.UUID;
  * Version: 1.0
  * Description: 03002001 文件上传接口实现类
  */
+@Slf4j
 @Service
 public class Serv03002001Impl implements Serv03002001 {
 
@@ -45,22 +48,27 @@ public class Serv03002001Impl implements Serv03002001 {
             pathExist.mkdirs();
         }
 
+        JSONArray fileList = new JSONArray();
+        JSONObject tmpSavedFile = null;
         /* 多文件上传 */
         for(MultipartFile file : files){
             fileHashCode = nowDay + "_" + UUID.randomUUID().toString();
             originalFilename = file.getOriginalFilename();// 上传的文件名
             fileFix = originalFilename.substring(originalFilename.lastIndexOf("."));
             filePath = filePathPre + fileHashCode + fileFix; //最终文件存储路径
+            tmpSavedFile = new JSONObject();
             try{
-                // 保存文件
-                file.transferTo(new File(filePath));
+                file.transferTo(new File(filePath)); // 保存文件
+                tmpSavedFile.put("saveResult", "1");
             } catch (IOException e) {
-                e.printStackTrace();
+                tmpSavedFile.put("saveResult", "0"); // 文件保存结果, 1-成功，0-失败
+                log.error("Serv03002001Impl [uploadFiles] 保存文件出现异常！fileName：{}, 异常信息: {}", originalFilename, e);
             }
-            resp.put(fileHashCode, filePath);
+            tmpSavedFile.put("fileIndex", fileHashCode);
+            tmpSavedFile.put("fileUrl", filePath);
+            fileList.add(tmpSavedFile);
         }
-
-        System.out.println("===》 " + resp);
+        resp.put("uploadSuccFiles", fileList);
         return resp;
     }
 }
